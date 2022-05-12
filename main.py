52ac41382700398f6988c8ba9bac4068c11caa36
loglog3 <loglog354@gmail.com>
2022-05-13 01:32:18 +0900

    리눅스 크롤링 옵션 추가

--- main.py
+++ main.py
@@ -25,14 +25,17 @@ def launchDriver():
     elif platform == 'linux':
         # TODO LABMDA
         chrome_options.add_argument('--disable-dev-shm-usage') # ??
+        chrome_options.add_argument('--incognito') # 시크릿모드
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--window-size=1280x960')
         chrome_options.add_argument('--user-data-dir=/tmp/user-data')
         chrome_options.add_argument('--hide-scrollbars')
-        chrome_options.add_argument('--enable-logging')
-        chrome_options.add_argument('--log-level=0')
+        # chrome_options.add_argument('--enable-logging')
+        # chrome_options.add_argument('--log-level=0')
+        chrome_options.add_argument('--disable-setuid-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--v=99')
         chrome_options.add_argument('--single-process')
         chrome_options.add_argument('--data-path=/tmp/data-path')
